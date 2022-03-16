9e175bee5868a3ee49eee9e1b219a93cf8497972
lintez2002 <101658970+lintez2002@users.noreply.github.com>
2022-03-16 15:23:57 +0530

    Add files via upload

---
+++ miniproject.py
@@ -0,0 +1,27 @@
+from pytube import YouTube
+
+
+link = "https://www.youtube.com/watch?v=EAYlckSaviI"
+
+youtube_1 = YouTube(link)
+
+
+#print(youtube_1.title)
+
+#print(youtube_1.thumbnail_url)
+
+
+videos = youtube_1.streams.all()
+
+vid = list(enumerate(videos))
+
+for i in vid:
+
+	print(i)
+
+print()
+
+strm = int(input("Enter : "))
+
+videos[strm].download()
+print('Successfully')
